4a728e74070617658fe9b2da95ab1ffbd55d89ea
xscode <stu.woodcock@gmail.com>
2018-12-18 15:22:39 +0000

    Speed and control char updates
    - Removed some unneeded sed commands from usenrmes.txt and domains.txt creation. 
    - Added removal of ^M control char that hashcat-legacy seems to use for end of line.

--- parse_email_results.sh
+++ parse_email_results.sh
@@ -6,8 +6,8 @@
 #                :outputs several useful files.
 #author		     :StuW
 #date            :15-12-2018
-#version         :0.1   
-#usage		       :parse_email_results.sh <hashcat output file>
+#version         :0.2
+#usage		     :parse_email_results.sh <hashcat output file>
 #===============================================================================
   
 SOURCE_FILE=$1
@@ -25,13 +25,13 @@ then
     # Remove the hash - split the email into each part required and sort based on frequency, save that to the frequency files
     # then remove teh frequency counts to create teh raw username and domain files in highest frequency order.
     echo "Parsing " $SOURCE_FILE " usernames into usernames.txt..."
-    cut -d':' -f2 $SOURCE_FILE | cut -s -d'@' -f1 | sort | uniq -c | sort -nr | sed 's/  */ /g' | cut -d' ' -f3 > usernames.txt
+    cut -d':' -f2 $SOURCE_FILE | cut -s -d'@' -f1 | sort | uniq > usernames.txt
     echo "Parsing " $SOURCE_FILE " domains into domains.txt..."
-    cut -d':' -f2 $SOURCE_FILE | cut -s -d'@' -f2 | sort | uniq -c | sort -nr | sed 's/  */ /g' | cut -d' ' -f3 > domains.txt
+    cut -d':' -f2 $SOURCE_FILE  | cut -d'@' -f2 | sort | sed 's/^M//g' | uniq> domains.txt
     echo "Parsing " $SOURCE_FILE " username frequencies into username_frequency.txt..."
-    cut -d':' -f2 $SOURCE_FILE | cut -s -d'@' -f1 | sort | uniq -c | sort -nr > domain_frequency.txt
+    cut -d':' -f2 $SOURCE_FILE | cut -s -d'@' -f1 | sort | uniq -c | sed 's/^M//g' | sort -nr > username_frequency.txt
     echo "Parsing " $SOURCE_FILE " domain frequencies into domain_frequency.txt..."
-    cut -d':' -f2 $SOURCE_FILE | cut -s -d'@' -f2 | sort | uniq -c | sort -nr > domain_frequency.txt
+    cut -d':' -f2 $SOURCE_FILE | cut -s -d'@' -f2 | sort | uniq -c | sed 's/^M//g' | sort -nr > domain_frequency.txt
     exit
 fi
 
